fc67ce0a95467fc3c8c8738b6923c5a754787185
k35o <kouki_sakano@dwango.co.jp>
2024-12-02 15:40:36 +0900

    deno fmt

--- day1/part1.ts
+++ day1/part1.ts
@@ -2,7 +2,7 @@ import * as path from "jsr:@std/path";
 import { readFileAsString } from "../utils.ts";
 
 const lines = (await readFileAsString(
-  path.join(import.meta.dirname ?? "", "input.txt")
+  path.join(import.meta.dirname ?? "", "input.txt"),
 )).split("\n");
 
 const left: number[] = [];
