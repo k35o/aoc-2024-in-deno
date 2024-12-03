ad0de2f93513ae2384052bd71e12e19d70320a1c
k35o <kouki_sakano@dwango.co.jp>
2024-12-03 17:05:43 +0900

    day3 part1

---
+++ day3/part1.ts
@@ -0,0 +1,19 @@
+import * as path from "jsr:@std/path";
+import { readFileAsString } from "../utils.ts";
+
+const lines = (await readFileAsString(
+  path.join(import.meta.dirname ?? "", "input.txt")
+)).split("\n");
+
+const regex = /mul\(\d+,\s*\d+\)/g;
+
+let result = 0;
+for (const line of lines) {
+  const matches = line.match(regex);
+  for (const match of matches ?? []) {
+    const [a, b] = match.slice(4, -1).split(",").map(Number);
+    result += a * b;
+  }
+}
+
+console.log(result);
