8be9b2e88f0b3b1d2ee6084ab89abcd8068d3f64
k35o <kouki_sakano@dwango.co.jp>
2024-12-11 14:53:48 +0900

    day11 part1

--- day11/part1.ts
+++ day11/part1.ts
@@ -1,6 +1,36 @@
 import * as path from "jsr:@std/path";
 import { readFileAsString } from "../utils.ts";
 
-const lines = (await readFileAsString(
+let lines = (await readFileAsString(
   path.join(import.meta.dirname ?? "", "input.txt"),
-)).split("\n");
+)).split(" ");
+
+const BREAKING = 25;
+
+for (let i = 0; i < BREAKING; i ++) {
+  let nextLines = lines;
+  let k = 0;
+  console.log(nextLines);
+  for (let j = 0; j < lines.length; j++) {
+    if (lines[j] === '0') {
+      nextLines[k] = '1';
+      k++;
+      continue;
+    }
+    if (lines[j].length % 2 === 0) {
+      nextLines = [
+        ...nextLines.slice(0, k),
+        String(Number(lines[j].slice(0, lines[j].length / 2))),
+        String(Number(lines[j].slice(lines[j].length / 2))),
+        ...nextLines.slice(k + 2),
+      ];
+      k += 2;
+      continue;
+    }
+    nextLines[k] = String(Number(lines[j]) * 2024);
+    k++;
+  }
+  lines = nextLines;
+}
+
+console.log(lines.length);
