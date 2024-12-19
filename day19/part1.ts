31ab2e6a59e191fa02c1ebdc175fa15a1993c9fc
Koki Sakano <kosakanoki@gmail.com>
2024-12-20 02:05:11 +0900

    day19

--- day19/part1.ts
+++ day19/part1.ts
@@ -3,4 +3,40 @@ import { readFileAsString } from "../utils.ts";
 
 const lines = (await readFileAsString(
   path.join(import.meta.dirname ?? "", "input.txt"),
-)).split("\n");
+)).split("\n\n");
+
+const candidates = lines[0].split(", ");
+const targets = lines[1].split("\n");
+
+let result = 0;
+for (const target of targets) {
+  let isComplete = false;
+  const checkedTarget = new Set<string>();
+  const re = (tar: string) => {
+    if (isComplete) {
+      return;
+    }
+    if (tar === "") {
+      isComplete = true;
+      return;
+    }
+    if (checkedTarget.has(tar)) {
+      return;
+    }
+
+    for (const candidate of candidates) {
+      if (tar.startsWith(candidate)) {
+        re(tar.slice(candidate.length));
+      }
+    }
+    checkedTarget.add(tar);
+  };
+
+  re(target);
+
+  if (isComplete) {
+    result++;
+  }
+}
+
+console.log(result);
--- day19/part2.ts
+++ day19/part2.ts
@@ -3,4 +3,36 @@ import { readFileAsString } from "../utils.ts";
 
 const lines = (await readFileAsString(
   path.join(import.meta.dirname ?? "", "input.txt"),
-)).split("\n");
+)).split("\n\n");
+
+const candidates = lines[0].split(", ");
+const targets = lines[1].split("\n");
+
+let result = 0;
+for (const target of targets) {
+  const targetMap = new Map<string, number>();
+  const re = (tar: string): number => {
+    if (targetMap.has(tar)) {
+      return targetMap.get(tar) ?? 0;
+    }
+
+    if (tar === "") {
+      return 1;
+    }
+
+    let count = 0;
+    for (const candidate of candidates) {
+      if (tar.startsWith(candidate)) {
+        count += re(tar.slice(candidate.length));
+      }
+    }
+
+    targetMap.set(tar, count);
+
+    return count;
+  };
+
+  result += re(target);
+}
+
+console.log(result);
